6564a7cd2521e9db841465050d89823617104fc4
sashwanth-7 <sashwanthyuvaraj@gmail.com>
2025-09-18 23:19:32 +0530

    Roadmap day1:python basics(print,variables,types)

---
+++ roadmap_day1.py
@@ -0,0 +1,32 @@
+print("hello,world")
+print("internship prep starts today")
+
+name="sashwanth"
+age=19
+height=6.0
+is_student=True
+
+print("name:",name)
+print("age:",age)
+print("height:",height)
+print("student:",is_student)
+
+x=10
+y=2.0
+z="python"
+a=True
+
+print("type of x:",type(x))
+print("type of y:",type(y))
+print("type of z:",type(z))
+print("type of a:",type(a))
+
+movie="interstellar"
+number=7
+percentage=73.7
+coding=True
+
+print("favourite movie:",movie)
+print("lucky number:",number)
+print("percentage:",percentage)
+print("likes coding:",coding)
